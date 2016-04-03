bb46413ed29242a4039618e638722de6d8ccaf0d
mdjones <mikedeanjones@yahoo.com>
2016-04-03 00:03:36 -0400

    Added in OptionParser

--- src/main/java/com/nibr/oncology/util/realwordid/RealWordIDUtil.java
+++ src/main/java/com/nibr/oncology/util/realwordid/RealWordIDUtil.java
@@ -1,14 +1,15 @@
 package com.nibr.oncology.util.realwordid;
 
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.core.env.SimpleCommandLinePropertySource;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 /**
  * CMD line app to work with the database
@@ -19,9 +20,16 @@ import org.springframework.stereotype.Component;
 public final class RealWordIdUtil {
     private static final Logger logger = LogManager.getLogger(RealWordIdUtil.class);
 
-    public static final String URL_PARAM_KEY = "url";
+    private final UniqueWordFetcher uniqueWordFetcher;
+    private final DictionaryDao dictionaryDao;
 
-    public static void main(String[] args) {
+    @Autowired
+    public RealWordIdUtil(final UniqueWordFetcher uniqueWordFetcher, DictionaryDao dictionaryDao) {
+        this.uniqueWordFetcher = uniqueWordFetcher;
+        this.dictionaryDao = dictionaryDao;
+    }
+
+    public static void main(String[] args) throws IOException, SQLException {
 
         // should use parameterless constructor as the other one invoke refresh which we certainly don't want
         // as it automatically trigger property injection and our CLI property is not ready yet
@@ -30,29 +38,37 @@ public final class RealWordIdUtil {
         // setup configuration
         applicationContext.register(Config.class);
 
-        // add CLI property source
-        applicationContext.getEnvironment().getPropertySources().addLast(new SimpleCommandLinePropertySource(args));
 
         // setup all the dependencies (refresh) and make them run (start)
         applicationContext.refresh();
         applicationContext.start();
 
         try {
-            String dbPath = applicationContext.getBean("databasePath", String.class);
-            if(dbPath == null){
-                usage("Please set " + Config.REAL_WORD_DB_PROP);
+            RealWordIdUtil realWordIdUtil = applicationContext.getBean(RealWordIdUtil.class);
+// add CLI property source
+            OptionParser parser = new OptionParser();
+            parser.accepts("rebuildDatabase", "Rebuild word table.");
+            parser.accepts("removeUsedWordList", "Rebuild used word table.");
+            parser.accepts("removeWordFromDataBase", "Remove the returned word from the database");
+            parser.accepts("randomWord").withOptionalArg().ofType( Integer.class ).defaultsTo(5);
+            OptionSet options = parser.parse(args);
+            //PropertySource ps = new JOptCommandLinePropertySource(options);
+            //applicationContext.getEnvironment().getPropertySources().addLast(ps);
 
-            }else {
-                logger.debug("SDFSF" + applicationContext.getBean("databasePath"));
-
-                RealWordIdUtil realWordIdUtil = applicationContext.getBean(RealWordIdUtil.class);
-                System.out.println("realWordIdUtil: " + realWordIdUtil);
-                UniqueWordFetcher uniqueWordFetcher
-                        = applicationContext.getBean("uniqueWordFetcher", UniqueWordFetcher.class);
-                System.out.println("uniqueWordFetcher: " + uniqueWordFetcher.getUniqueWord(9, false));
+            //logger.debug(applicationContext.getEnvironment());
+            if(options.has("randomWord")){
+                int wordSize = Integer.parseInt(options.valueOf("randomWord").toString());
+                boolean removeFromDataBase = options.has("removeWordFromDataBase");
+                realWordIdUtil.printRandomWord(wordSize, removeFromDataBase);
+            }
+            if(options.has("rebuildDatabase")){
+                realWordIdUtil.rebuildDataBase();
             }
 
-
+            if(!options.hasOptions() || options.has("help")) {
+                parser.printHelpOn(System.out);
+                System.exit(0);
+            }
 
         } finally {
             applicationContext.close();
@@ -60,7 +76,12 @@ public final class RealWordIdUtil {
 
     }
 
-    private static void usage(String s) {
-        System.err.println(s);
+    private void rebuildDataBase() throws IOException, SQLException {
+        dictionaryDao.rebuildUsedWordsTable();
+        dictionaryDao.createDataBase();
+    }
+
+    private void printRandomWord(int wordSize, boolean removeFromDataBase) {
+        logger.info("The word is " + uniqueWordFetcher.getUniqueWord(wordSize, removeFromDataBase));
     }
 }
